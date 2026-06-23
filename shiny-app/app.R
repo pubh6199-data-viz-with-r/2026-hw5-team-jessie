09ffbd5860935738fb78359891517d8a7968ea3a
meganyliu <megan.liu@gwmail.gwu.edu>
2026-06-23 00:47:35 -0400

    Megan 12:47a 6.23 update

--- shiny-app/app.R
+++ shiny-app/app.R
@@ -1,1 +1,16 @@
 # placeholder for shiny app
+
+library(shiny)
+
+# ui <- fluidPage()
+
+# server <- function(input, output, session) {}
+
+
+
+
+
+
+
+# final line of code below:
+# shinyApp(ui, server)
